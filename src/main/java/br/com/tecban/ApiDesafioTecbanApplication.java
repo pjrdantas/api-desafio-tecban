1c0a8f46721e4f0271360edfcbe4c5bbe852bc52
pjrdantas <pjr_dantas@hotmail.com>
2021-03-20 00:13:10 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/tecban/ApiDesafioTecbanApplication.java
@@ -0,0 +1,18 @@
+package br.com.tecban;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDesafioTecbanApplication {
+	
+	private static final Logger log = LoggerFactory.getLogger(ApiDesafioTecbanApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDesafioTecbanApplication.class, args);
+		log.info("********** API-DESAFIO-TECBAN ESTA PRONTO PARA SER CONSUMIDO! **********");
+	}
+
+}
---
+++ src/main/java/br/com/tecban/dto/package-info.java
@@ -0,0 +1,1 @@
+package br.com.tecban.dto;
